fb3b31a79ded95044c2b70cea9afb2a6f3e0692a
yurika0501 <119822953+yurika0501@users.noreply.github.com>
2023-01-12 12:39:54 +0000

    [Add]ユーザー詳細ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
+  # get 'users/show'
+  # get 'users/edit'
   devise_for :users
+  root to: "homes#top"
+  get 'homes/about', to: 'homes#about' , as: 'about'
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
-  get 'homes/about', to: 'homes#about' , as: 'about'
-
+  resources :users, only: [:show, :edit]
 end
