acd3c4003591796d1b15ec1256e8f5a9e969d7e0
yurika0501 <119822953+yurika0501@users.noreply.github.com>
2023-01-13 02:09:26 +0000

    [Add]プロフィールのログイン機能

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,5 @@
 class PostImage < ApplicationRecord
-  
+
   has_one_attached :image
 
   #userモデルとの関連付け
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'homes/about', to: 'homes#about' , as: 'about'
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 end
